df1674d8aa7077d8cdbb65cea76b7c682adebe28
Giang Văn Lực <ekkojayce3108@gmail.com>
2021-11-16 19:15:59 +0700

    Export data script

--- SQL-query/delete_story.sql
+++
@@ -1,5 +0,0 @@
-CREATE PROCEDURE `delete_story` (in story_id varchar(36))
-BEGIN
-	Delete from story_chapters where story_id = story_id;
-    Delete from story where id = story_id;
-END
--- SQL-query/post_comment_procedure.sql
+++
@@ -1,6 +0,0 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `post_comment`(in i_story_id varchar(36), in i_email varchar(50), in i_content text)
-BEGIN
-	Declare inserting_index smallint;
-    Select max(`index`)+1 into inserting_index from story_comment where story_id = i_story_id and email = i_email; 
-    Insert into story_comment(story_id, email, `index`, content) Values(i_story_id, i_email, inserting_index, i_content);
-END
